a9621718e2cc0087e6f6d8d550c558c6c66a121a
Charity Samuel <135695024+charsamuel@users.noreply.github.com>
2024-05-24 12:07:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+// app.get("/api/products/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findById(id);
+//     res.status(200).json(product);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+// app.post("/api/products", async (req, res) => {
+//   try {
+//     const product = await Product.create(req.body);
+//     res.status(200).json(product);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+//Update a product
+// app.put("/api/products/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findByIdAndUpdate(id, req.body);
+//     if (!product) {
+//       return res.status(404).json({ message: "Product not found" });
+//     }
+//     const updatedProduct = await Product.findById(id);
+//     res.status(200).json(updatedProduct);
+//   } catch {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+//Delete a product
+
+// app.delete("/api/products/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findByIdAndDelete(id);
+//     if (!product) {
+//       return res.status(404).json({ message: "Product not found" });
+//     }
+//     res.status(200).json({ message: "Product deleted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:Foradmin2030@backenddb.btjxol9.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+    app.listen(9000, () => {
+      console.log("Server is running on port 9000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
---
+++ routes/product.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product.model.js");
+const {getProducts,getProduct,createProduct,updateProduct,deleteProduct} = require("../controller/product.controller.js")
+
+router.get("/",getProducts);
+router.get("/:id",getProduct)
+
+router.post("/",createProduct)
+
+//Update a product
+router.put("/:id",updateProduct);
+
+//Delete a product
+router.delete("/:id", deleteProduct)
+
+
+module.exports = router;
